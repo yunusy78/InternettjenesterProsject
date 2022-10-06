90a5af85720405d9b502e9ed0ef6abf0fe0ffccb
Yunus Yigit <Yunusy@uia.no>
2022-10-06 11:18:51 +0200

    yunus

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProsjectInternet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
